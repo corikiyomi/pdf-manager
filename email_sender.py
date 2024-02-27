94c35dff5816b3d4710b9f40ad315d0ba6fb53c4
Cori Komiyama <corikiyomi@gmail.com>
2024-02-27 15:30:38 -0700

    Modified email_sender.py to send customized emails using html, Path, and Template.

--- email_sender.py
+++ email_sender.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('email.html').read_text())
 email = EmailMessage()
 email['from'] = 'Cori Komiyama'
 email['to'] = 'cori.komiyama@gmail.com'
 email['subject'] = 'Response needed immediately!'
 
-email.set_content('This is a message asking you about your car\'s extended warranty')
+email.set_content(html.substitute(name='Molly'), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp: #as protocol, this port and syntax is standard
     smtp.ehlo()
